53ccb270bf1c2aa4cf1475959003788f0f7141e8
Sheplu <jean.burellier@gmail.com>
2017-02-10 13:44:40 +0100

    empty repository

--- README.md
+++
@@ -1,2 +0,0 @@
-# DemoTest
-demo repository
--- express/models/todo.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var Todo = new Schema({
-	message: String,
-	title: String
-});
-
-module.exports = mongoose.model('Todo', Todo)
